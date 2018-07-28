88db48708a1de798e4a6f499ac7e3cfba4b9ae25
Alynie <41819157+Alynie@users.noreply.github.com>
2018-07-28 13:17:29 -0500

    Add files via upload

---
+++ HarryPotterGlove.ino
@@ -0,0 +1,96 @@
+/*For the sound glove, this code plays the Harry Potter Theme
+ * when the glove is closed and no sound when the glove is open
+ */
+// variables to store the bend sensor and buzzer pins
+
+int bendSensorPin = A3;
+
+// variable to store the values from the bend sensor
+int bendSensorReading;
+
+struct MusicStruct {
+  int A = 550;
+  int As = 582;
+  int B = 617;
+  int C = 654;
+  int Cs = 693;
+  int D = 734;
+  int Ds = 777;
+  int E = 824;
+  int F = 873;
+  int Fs = 925;
+  int G = 980;
+  int Gs = 1003;
+  int A2 = 1100;
+  int A2s = 1165;
+  int B2 = 1234;
+  int C3 = 1308;
+  int C3s = 1385;
+  int D3 = 1555;
+}Music;
+
+struct LengthStruct {
+  float half = 0.5;
+  float one = 1.0;
+  float one_half = 1.5;
+  float two = 2.0;
+  float two_half = 2.5;
+}Length;
+
+int tempo = 400;
+int buzzer_pin = 9;
+
+void setup() {
+
+  //Tell Arduino we want this pin to be an output
+  pinMode(buzzer_pin, OUTPUT);
+
+  //We need to activate a special functionality to reduce the
+  //noise in our sensor. If you don't use INPUT_PULLUP
+  pinMode(bendSensorPin, INPUT_PULLUP);
+
+  //Start the serial monitor
+  Serial.begin(9600);
+
+}
+
+void setTone(int pin, int note, int duration) {
+  tone(pin, note, duration);
+  delay(duration);
+  noTone(pin);
+}
+
+void loop() {
+
+  // read the sensor:
+  bendSensorReading = analogRead(bendSensorPin);
+
+  // print the sensor reading so you know its range
+  Serial.print("Raw bend sensor reading = ");
+  Serial.print(bendSensorReading);
+  Serial.print("\t");
+
+if (bendSensorReading < 150 || bendSensorReading > 350) {delay(500);}
+else if (150 < bendSensorReading < 350)
+{
+  setTone(buzzer_pin, Music.B, tempo * Length.one);
+  setTone(buzzer_pin, Music.E, tempo * Length.one_half);
+  setTone(buzzer_pin, Music.G, tempo * Length.half);
+  setTone(buzzer_pin, Music.F, tempo * Length.one);
+  setTone(buzzer_pin, Music.E, tempo * Length.two);
+  setTone(buzzer_pin, Music.B2, tempo * Length.one);
+  setTone(buzzer_pin, Music.A2, tempo * Length.two_half);
+  setTone(buzzer_pin, Music.Fs, tempo * Length.two_half);
+  
+  setTone(buzzer_pin, Music.E, tempo * Length.one_half);
+  setTone(buzzer_pin, Music.G, tempo * Length.half);
+  setTone(buzzer_pin, Music.F, tempo * Length.one);
+  setTone(buzzer_pin, Music.Ds, tempo * Length.two);
+  setTone(buzzer_pin, Music.F, tempo * Length.one);
+  setTone(buzzer_pin, Music.B, tempo * Length.two_half);
+  delay(500);
+}
+else {
+delay(3000);}
+delay(500);
+}
